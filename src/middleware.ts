4983c46aabe0e7fd870a56969f6f573e7148c61f
gheodn43 <97235672+gheodn43@users.noreply.github.com>
2024-09-15 10:52:20 +0700

    update: middleware

---
+++ src/middleware.ts
@@ -0,0 +1,21 @@
+import { NextRequest, NextResponse } from 'next/server';
+
+export function middleware(request: NextRequest) {
+//   const token = request.cookies.get('refreshToken');
+//   const redirectUrl = new URL('/signin', request.url);
+
+//   if (!token && request.nextUrl.pathname !== '/signin') {
+//     redirectUrl.searchParams.set('redirect', request.nextUrl.pathname);
+//     return NextResponse.redirect(redirectUrl);
+//   }
+
+//   if (token && request.nextUrl.pathname === '/signin') {
+//     return NextResponse.redirect(new URL('/', request.url));
+//   }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ['/home/:path*'],
+};
